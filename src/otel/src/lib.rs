ef5068a8f8827ec26916d07b25532dd61faee2fe
Gabor Javorszky <g.javorszky@f5.com>
2024-07-31 21:08:00 +0100

    Use reqwest async to send single traces to otel

--- src/otel/src/lib.rs
+++ src/otel/src/lib.rs
@@ -1,15 +1,14 @@
-use std::sync::{Arc, OnceLock};
 use std::ffi::{CStr, CString};
 use std::ptr;
+use std::sync::{Arc, OnceLock};
 use std::time::Duration;
-
-//use opentelemetry::global;
-//use opentelemetry::global::{BoxedTracer, BoxedSpan};
-use opentelemetry::trace::{SpanKind, SpanBuilder, TraceId, Span, Tracer, TracerProvider};
-use opentelemetry_sdk::trace::{Span as SpanImpl, TracerProvider as TracerProviderImpl};
-use opentelemetry_sdk::Resource;
+use opentelemetry::trace::{
+    Span, SpanBuilder, SpanKind, TraceId, Tracer, TracerProvider,
+};
 use opentelemetry::KeyValue;
-use opentelemetry_otlp::{WithExportConfig, Protocol::HttpBinary};
+use opentelemetry_otlp::{Protocol::HttpBinary, WithExportConfig};
+use opentelemetry_sdk::trace::{Config, Span as SpanImpl, TracerProvider as TracerProviderImpl};
+use opentelemetry_sdk::Resource;
 
 // otel_endpoint is hardcoded for proof of concept purposes.
 const OTEL_TRACES_ENDPOINT: &str = "http://lgtm:4318/v1/traces";
@@ -17,46 +16,45 @@ const OTEL_TRACES_ENDPOINT: &str = "http://lgtm:4318/v1/traces";
 static GLOBAL_TRACER_PROVIDER: OnceLock<TracerProviderImpl> = OnceLock::new();
 
 // potentially returns an error message
-// TODO: pass in a callback to log error instead of broken bullshit
 #[no_mangle]
 unsafe fn nxt_otel_init(log_callback: unsafe extern "C" fn(*mut i8)) {
     let otlp_exporter = opentelemetry_otlp::new_exporter()
         .http()
-        .with_endpoint(String::from(OTEL_TRACES_ENDPOINT))
+        .with_endpoint(OTEL_TRACES_ENDPOINT)
         .with_protocol(HttpBinary)
-        .with_timeout(Duration::new(10,0));
+        .with_timeout(Duration::new(10, 0));
 
     // Then pass it into pipeline builder
     let res = opentelemetry_otlp::new_pipeline()
         .tracing()
-        .with_trace_config(
-            opentelemetry_sdk::trace::config().with_resource(Resource::new(vec![KeyValue::new(
-                opentelemetry_semantic_conventions::resource::SERVICE_NAME, "NGINX Unit",
-            )])),
-        )
+        .with_trace_config(Config::default().with_resource(
+            Resource::new(vec![KeyValue::new(
+                opentelemetry_semantic_conventions::resource::SERVICE_NAME,
+                "NGINX Unit",
+            )]),
+        ))
         .with_exporter(otlp_exporter)
         .install_simple();
 
     // unwrap
     match res {
         Err(e) => log_callback(
-            CString::from_vec_unchecked(
-                e.to_string()
-                    .as_bytes()
-                    .to_vec()
-            ).into_raw() as _
+            CString::from_vec_unchecked(e.to_string().as_bytes().to_vec())
+                .into_raw() as _,
         ),
         Ok(t) => {
             GLOBAL_TRACER_PROVIDER.get_or_init(move || t);
-            log_callback(CString::from_vec_unchecked(
-                "otel exporter has been initialised".as_bytes().to_vec()
-            ).into_raw() as _);
+            log_callback(
+                CString::from_vec_unchecked(
+                    "otel exporter has been initialised".as_bytes().to_vec(),
+                )
+                .into_raw() as _,
+            );
         }
     }
 }
 
-
-// its on the caller to pass in a buf of proper length
+// it's on the caller to pass in a buf of proper length
 #[no_mangle]
 pub unsafe fn nxt_otel_copy_traceparent(buf: *mut i8, span: *const SpanImpl) {
     if buf.is_null() || span.is_null() {
@@ -66,13 +64,17 @@ pub unsafe fn nxt_otel_copy_traceparent(buf: *mut i8, span: *const SpanImpl) {
     let traceparent = format!(
         "00-{:032x}-{:016x}-{:02x}",
         (*span).span_context().trace_id(), // 16 chars, 32 hex
-        (*span).span_context().span_id(), // 8 byte, 16 hex
-        (*span).span_context().trace_flags() // 1 char, 2 hex
+        (*span).span_context().span_id(),  // 8 byte, 16 hex
+        (*span).span_context().trace_flags()  // 1 char, 2 hex
     );
 
-    assert!(traceparent.len() == 55);
+    assert_eq!(traceparent.len(), 55);
 
-    std::ptr::copy_nonoverlapping(traceparent.as_bytes().as_ptr(), buf as _, 55);
+    ptr::copy_nonoverlapping(
+        traceparent.as_bytes().as_ptr(),
+        buf as _,
+        55,
+    );
     // set null terminator
     *buf.add(54) = b'\0' as _;
 }
@@ -83,32 +85,22 @@ pub unsafe fn nxt_otel_add_event_to_trace(
     key: *mut i8,
     val: *mut i8,
 ) {
-    if !key.is_null() &&
-        !val.is_null() &&
-        !trace.is_null() {
-            let key = CStr::from_ptr(key as _)
-                .to_string_lossy();
-            let val = CStr::from_ptr(val as _)
-                .to_string_lossy();
+    if !key.is_null() && !val.is_null() && !trace.is_null() {
+        let key = CStr::from_ptr(key as _).to_string_lossy();
+        let val = CStr::from_ptr(val as _).to_string_lossy();
 
-            (*trace).add_event(
-                String::from("from_c"),
-                vec![KeyValue::new(key, val)]
-             );
-        }
+        (*trace)
+            .add_event(String::from("from_c"), vec![KeyValue::new(key, val)]);
+    }
 }
 
 #[no_mangle]
-pub unsafe fn nxt_otel_end_span(
-    trace: *mut SpanImpl,
-) {
+pub unsafe fn nxt_otel_end_span(trace: *mut SpanImpl) {
     (*trace).end();
 }
 
 #[no_mangle]
-pub unsafe fn nxt_otel_get_or_create_trace(
-    trace_id: *mut i8
-) -> *mut SpanImpl {
+pub unsafe fn nxt_otel_get_or_create_trace(trace_id: *mut i8) -> *mut SpanImpl {
     let mut trace_key = None;
     let trace_cstr: &CStr;
     if !trace_id.is_null() {
@@ -129,12 +121,12 @@ pub unsafe fn nxt_otel_get_or_create_trace(
         return ptr::null::<SpanImpl>() as *mut SpanImpl;
     }
 
-    let arc_span = Arc::new(span);
-    return Arc::<SpanImpl>::into_raw(arc_span) as *mut SpanImpl;
+    Arc::<SpanImpl>::into_raw(Arc::new(span)) as *mut SpanImpl
 }
 
 #[no_mangle]
-pub unsafe fn nxt_otel_send_trace(trace: *mut SpanImpl) {
+#[tokio::main]
+pub async unsafe fn nxt_otel_send_trace(trace: *mut SpanImpl) {
     // damage nothing on an improper call
     if trace.is_null() {
         eprintln!("trace was null, returning");
